c73d00be1479e1055060e5d0eb0cf0bf55e8809b
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-09-06 21:12:30 -0400

    fix

--- na.js
+++ na.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const contenedor = document.getElementById("navbarr");
   const enlace = document.createElement("a");
 
-  enlace.href = "https://lagunalawnservice.com/lawn%20mowing";
-  enlace.textContent = "Lawn Mowing";
+  enlace.href = "https://lagunalawnservice.com/learn";
+  enlace.textContent = "Miguel Laguna";
 
   contenedor.appendChild(enlace);
 });
